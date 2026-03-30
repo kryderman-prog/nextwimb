6c73a25fe33b2aff349314e5d2044b149fec96da
kryderman-prog <kryderman-prog@github.com>
2026-03-30 18:21:07 +0530

    commit 4

--- services/authService.ts
+++ services/authService.ts
@@ -15,7 +15,9 @@ export class AuthService {
     const { error } = await this.supabase.auth.signInWithOAuth({
       provider: 'google',
       options: {
-        redirectTo: `${window.location.origin}/auth/callback`,
+        // redirectTo: `${window.location.origin}/auth/callback`,
+        redirectTo: `${process.env.NEXT_PUBLIC_SITE_URL}/auth/callback`,
+
       },
     })
 
